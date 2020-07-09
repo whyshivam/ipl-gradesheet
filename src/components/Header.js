5680b7ed78d0f6ba443db425e6c1f73e291c1fcd
Shivam Kumar <shivamkr.vatsa@gmail.com>
2020-07-09 22:59:57 +0530

    Deploy

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,56 +1,71 @@
 import React, { Component } from 'react';
-import {Nav, NavItem,NavLink,Navbar,NavbarBrand, Collapse, NavbarToggler, Jumbotron} from 'reactstrap';
-import {Link , animateScroll as scroll} from 'react-scroll'
-class Header extends Component{
-    constructor(props){
+import { Nav, NavItem, Navbar, NavbarBrand, Collapse, NavbarToggler, Jumbotron } from 'reactstrap';
+import { Link, animateScroll as scroll } from 'react-scroll'
+class Header extends Component {
+    constructor(props) {
         super(props);
-        this.state={
-            isNavOpen : false
+        this.state = {
+            isNavOpen: false
         }
-        this.toggleNav=this.toggleNav.bind(this);
+        this.toggleNav = this.toggleNav.bind(this);
     }
-    toggleNav(){
+    toggleNav() {
         this.setState({
             isNavOpen: !this.state.isNavOpen
         });
     }
 
-    render(){
-        return(
+    render() {
+        return (
             <React.Fragment>
-               
+
                 <Navbar color="light" light expand="md" className="header" >
-                <div className="container">
-                    <NavbarBrand href="/ipl-gradesheet/">IPL Grade</NavbarBrand>
-                    <NavbarToggler onClick={this.toggleNav}/>
-                    <Collapse isOpen={this.state.isNavOpen} navbar>
-                        <Nav navbar >
-                        <NavItem>
-                                <NavLink className="nav-link" href="/ipl-gradesheet/#win">
-                                    Winning Grade
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" href="/ipl-gradesheet/#team">
-                                    Team Grade
-                                </NavLink>
-                            </NavItem>
-                            <NavItem>
-                                <NavLink className="nav-link" href="/ipl-gradesheet/#season">
-                                    Season Grade
-                                </NavLink>
-                            </NavItem>
-                            
-                            
-                        </Nav>
-                    </Collapse>
+                    <div className="container">
+                        <NavbarBrand href="/ipl-gradesheet/">IPL Grade</NavbarBrand>
+                        <NavbarToggler onClick={this.toggleNav} />
+                        <Collapse isOpen={this.state.isNavOpen} navbar>
+                            <Nav navbar >
+                                <NavItem>
+                                    <Link
+                                        activeClass="active"
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-70}
+                                        duration={500} className="nav-link" to="win">
+                                        Winning Grade
+                                </Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link
+                                        activeClass="active"
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-70}
+                                        duration={500} className="nav-link" to="team">
+                                        Team Grade
+                                </Link>
+                                </NavItem>
+                                <NavItem>
+                                    <Link
+                                        activeClass="active"
+                                        spy={true}
+                                        smooth={true}
+                                        offset={-70}
+                                        duration={500} className="nav-link" to="season">
+                                        Season Grade
+                                </Link>
+                                </NavItem>
+
+
+                            </Nav>
+                        </Collapse>
                     </div>
                 </Navbar>
                 <Jumbotron fluid>
-                    <div  className="container jumbo">
+                    <div className="container jumbo">
                         <div className=" jumbo-head col-12">
                             <span>
-                            <h1 className="display-3">Indian Premiere League</h1>
+                                <h1 className="display-3">Indian Premiere League</h1>
                             </span>
                         </div>
                     </div>
@@ -58,7 +73,7 @@ class Header extends Component{
             </React.Fragment>
         );
     }
-} 
+}
 
 export default Header;
 
