5e343fdfbe30f5046fc7b9283b7ab8baa2b3e1bd
Shivam Kumar <shivamkr.vatsa@gmail.com>
2020-07-09 13:33:34 +0530

    Updates

---
+++ precache-manifest.1d23602b662395a12425e93095e20c48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e083f9a8d543441350f9680efadffa6",
+    "url": "/ipl-gradesheet/index.html"
+  },
+  {
+    "revision": "1dd604a1a923fdeba01c",
+    "url": "/ipl-gradesheet/static/css/2.42836a8c.chunk.css"
+  },
+  {
+    "revision": "c56cfbc2da3f425b8f8b",
+    "url": "/ipl-gradesheet/static/css/main.bbcf8962.chunk.css"
+  },
+  {
+    "revision": "1dd604a1a923fdeba01c",
+    "url": "/ipl-gradesheet/static/js/2.d03140b4.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/ipl-gradesheet/static/js/2.d03140b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c56cfbc2da3f425b8f8b",
+    "url": "/ipl-gradesheet/static/js/main.18e486b9.chunk.js"
+  },
+  {
+    "revision": "20e6a4084f301615803a",
+    "url": "/ipl-gradesheet/static/js/runtime-main.0d612c6b.js"
+  }
+]);
